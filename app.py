2dbdfc27d88b1a9d03c8e94409315240fb33babd
Lingvistik <emil1811@gmail.com>
2020-07-16 01:22:18 +0100

    Python app initialized, base and template htmls added, mongodb connected, password placed in environment variable

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+from pymongo import MongoClient
+
+db_pass = os.environ.get('DB_PASS')
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'songbook'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://Lingvistik:'+str(db_pass)+'@prviklaster-v3qci.mongodb.net/songbook?retryWrites=true&w=majority')
+
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def route():
+    return redirect(url_for('get_chords'))
+
+@app.route('/get_chords')
+def get_chords():
+    return render_template('songbook.html', chords=mongo.db.chords.find())
+
+@app.route('/add_chords')
+def add_chords():
+    return render_template('addchords.html', genre=mongo.db.genre.find())
+
+@app.route('/chords_diagram')
+def chords_diagram():
+    return render_template('chordsdiagram.html')
+
+@app.route('/insert_chords', methods=['POST'])
+def insert_chords():
+    chords = mongo.db.chords
+    chords.insert_one(request.form.to_dict())
+    return redirect(url_for('get_chords'))
