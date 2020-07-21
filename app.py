6faee6a3d69252fa989e7638844b5baf7535134e
Lingvistik <emil1811@gmail.com>
2020-07-21 20:51:47 +0100

    Edit functionality added, style of the page in testing fase, preparation for images upload

--- app.py
+++ app.py
@@ -31,17 +31,33 @@ def show_chords(chord_id):
 def add_chords():
     return render_template('addchords.html', genre=mongo.db.genre.find())
 
-@app.route('/chords_diagram')
-def chords_diagram():
-    return render_template('chordsdiagram.html')
-
 @app.route('/insert_chords', methods=['POST'])
 def insert_chords():
     chords = mongo.db.chords
     chords.insert_one(request.form.to_dict())
     return redirect(url_for('get_chords'))
 
+@app.route('/edit_chords/<chord_id>')
+def edit_chords(chord_id):
+    the_chord = mongo.db.chords.find_one({"_id": ObjectId(chord_id)})
+    return render_template('editchords.html', chords=the_chord,)
+
+@app.route('/update_chords/<chord_id>', methods=['POST'])
+def update_chords(chord_id):
+    chords = mongo.db.chords
+    chords.update({'_id': ObjectId(chord_id)},
+    {
+        'band_name':request.form.get('band_name'),
+        'song_name':request.form.get('song_name'),
+        'chords_text': request.form.get('chords_text'),
+    })
+    return redirect(url_for('get_chords'))
+
 @app.route('/delete_chords/<chord_id>')
 def delete_chords(chord_id):
     mongo.db.chords.remove({'_id': ObjectId(chord_id)})
     return redirect(url_for('get_chords'))
+
+@app.route('/chords_diagram')
+def chords_diagram():
+    return render_template('chordsdiagram.html')
